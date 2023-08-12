96a43083ebea679130a8a54e6b2db0125b093769
Ryan Carey <carey-ryan@hotmail.com>
2023-08-11 23:09:46 -0700

    Component redesign/split for better reusability. Put landing page together. Let me know what you think

--- src/Components/ProblemSection/ProblemSection.tsx
+++ src/Components/ProblemSection/ProblemSection.tsx
@@ -4,6 +4,7 @@ import {
   rem,
 } from '@mantine/core';
 import React from 'react';
+import { Backhoe, Mailbox, Clock2, ClockHour7, ClockHour8 } from 'tabler-icons-react';
 
 const useStyles = createStyles((theme) => ({
 
@@ -46,8 +47,7 @@ const useStyles = createStyles((theme) => ({
 
   vertline: {
     width: '230px',
-    height: '1px',
-    background: '#e65e8c',
+    borderBottom: '2px dotted #e65e8c',
     position: 'relative',
     transform: 'Rotate(90deg)',
     left: '24%',
@@ -56,28 +56,25 @@ const useStyles = createStyles((theme) => ({
 
   horizline1: {
     width: '35px',
-    height: '1px',
-    background: '#e65e8c',
+    borderBottom: '2px dotted #e65e8c',
     position: 'relative',
-    left: '31.95%',
+    left: '31.91%',
     top: 137,
   },
 
   horizline2: {
       width: '550px',
-      height: '1px',
-      background: '#e65e8c',
+      borderBottom: '2px dotted #e65e8c',
       position: 'relative',
-      left: 420,
+      left: 419.7,
       top: 253
     },
 
   horizline3: {
     width: '35px',
-    height: '1px',
-    background: '#e65e8c',
+    borderBottom: '2px dotted #e65e8c',
     position: 'relative',
-    left: '31.95%',
+    left: '31.91%',
     top: 365,
   },
 
@@ -86,7 +83,8 @@ const useStyles = createStyles((theme) => ({
     height: '500px',
     position: 'relative',
     display: 'flex',
-    margin: '0 auto'
+    margin: '0 auto',
+    marginBottom: '40px'
   },
 
   titlediv: {
@@ -111,14 +109,14 @@ const useStyles = createStyles((theme) => ({
   },
 
   stats: {
-    color: 'rgb(255,255,255, .7)',
+    color: 'black',
     width: '400px',
     height: '150px',
     display: 'flex',
     alignItems: 'center',
     textAlign: 'center',
-    padding: '25px',
-    background: '#333',
+    padding: '20px',
+    backgroundColor: 'white',
     boxShadow: '5px 5px 15px black',
     borderRadius: '10px',
     margin: '0 auto',
@@ -139,18 +137,40 @@ export default function ProblemSection() {
           <div className={classes.horizline2}></div>
           <div className={classes.horizline3}></div>
           <div className={classes.statscontainer}>
-              <div className={classes.titlediv}>
-                  <Text fw={225} size={rem(30)} className={classes.title}>The average employee</Text>
+            <div className={classes.titlediv}>
+              <Text fw={250} size={rem(30)} className={classes.title}>The average employee</Text>
+            </div>
+            <div className={classes.statsdiv}>
+              <div className={classes.stats}>
+                <div>
+                  <Backhoe size={50} strokeWidth={0.8} />
+                </div>
+                <div>
+                  <Text fw={200} size={rem(20)}>Has 62% of their inbox filled with unimportant emails that can be processed in bulk.</Text>
+                </div>
               </div>
-              <div className={classes.statsdiv}>
-                  <Text fw={200} size={rem(20)} className={classes.stats}>Loses 2.5 hours per day due to ineffective communication and collaboration in the workplace</Text>
-                  <Text fw={200} size={rem(20)} className={classes.stats}>Spends 17 hours per week reading and sending work emails but 30% of these emails are neither urgent nor important </Text>
+              <div className={classes.stats}>
+                <div>
+                  <ClockHour8 size={50} strokeWidth={0.8} />
+                </div>
+                <div>
+                  <Text fw={200} size={rem(20)}>Spends 10% of the total time on email organizing messages they want to keep.</Text>
+                </div>
+              </div>  
+            </div>
+            <div className={classes.statsdiv}>
+              <div className={classes.stats}>
+                <div>
+                  <Mailbox size={50} strokeWidth={0.8} />
+                </div>
+                <div>
+                  <Text fw={200} size={rem(20)}>Wastes 27 minutes a day due to a full and unorganized inbox.</Text>
+                </div>
               </div>
-              <div className={classes.statsdiv}>
-                  <Text fw={200} size={rem(20)} className={classes.stats}>Receives 74 emails during the workday while only sending 26 emails</Text>
-              </div>
+            </div>
           </div>
       </div>
     </div>
   );
 }
+
--- src/Components/mainPage/Features/Features.tsx
+++ src/Components/mainPage/Features/Features.tsx
@@ -8,29 +8,6 @@ import {
   } from '@mantine/core';
   import React from 'react';
   import { Axe, Coin, HourglassHigh, Files } from 'tabler-icons-react';
-  
-  const stepdata = [
-    {
-      title: 'Step 1',
-      description:
-        'Seamlessly organize thousands of emails into custom folders with a simple click.',
-    },
-    {
-      title: 'Step 2',
-      description:
-        'Overwhelmed by junk emails? Instantly opt-out from every unwanted message in your junk folder.',
-    },
-    {
-      title: 'Step 3',
-      description:
-        'Streamline your email sorting process from hours to minutes with Zero Inbox.',
-    },
-    {
-      title: 'Step 4',
-      description:
-        'Spend your new found free time however you want',
-    },
-  ];
 
   const featuredata = [
     {
@@ -60,14 +37,6 @@ import {
   ];
   
   const useStyles = createStyles((theme) => ({
-    title: {
-      fontSize: rem(34),
-      fontWeight: 300,
-  
-      [theme.fn.smallerThan('sm')]: {
-        fontSize: rem(24),
-      },
-    },
 
     wrapper: {
       backgroundColor: "#333",
@@ -77,40 +46,9 @@ import {
       justifyContent: 'center',
       alignItems: 'center',
       padding: '45px',
+      paddingTop: '80px'
     },  
 
-    stepsheading: {
-      margin: '0 auto',
-      color: 'white',
-      textAlign: 'center',
-
-      '&::after': {
-        content: '""',
-        display: 'block',
-        backgroundColor: "#E65E8C",
-        width: rem(45),
-        height: rem(2),
-        marginTop: theme.spacing.sm,
-        marginLeft: 'auto',
-        marginRight: 'auto',
-      },
-    },
-
-    stepscard: {
-      width: '33%',
-      height: '200px',
-      background: "#333",
-      boxShadow: '5px 5px 15px black'
-    },
-
-    stepstitle: {
-      color: "#e65e8c"
-    },
-
-    stepsdescription: {
-      color: 'white'
-    },
-  
     featuresdescription: {
       maxWidth: 800,
       margin: '0 auto',
@@ -129,11 +67,11 @@ import {
     },
   
     featurecard: {
-      backgroundColor: '#e8e8e8',
+      backgroundColor: 'white',
       boxShadow: '2px 2px 10px black',
     },
   
-    cardTitle: {
+    cardtitle: {
       '&::after': {
         content: '""',
         display: 'block',
@@ -151,42 +89,23 @@ import {
     const features = featuredata.map((feature) => (
       <Card key={feature.title} shadow="md" radius="md" className={classes.featurecard} padding="xl">
         <feature.icon size={rem(50)} strokeWidth={1.25} color={'#333'} />
-        <Text fz="lg" fw={225} className={classes.cardTitle} mt="md">
+        <Text fz="lg" fw={250} className={classes.cardtitle} mt="md">
           {feature.title}
         </Text>
-        <Text fz="sm" fw={225} c="dimmed" mt="sm">
+        <Text fz="sm" fw={250} c='dimmed' mt="sm">
           {feature.description}
         </Text>
       </Card>
     ));
 
-    const steps = stepdata.map((step) => (
-      <Card key={step.title} radius="md" className={classes.stepscard} padding="xl">
-        <Text fz="lg" fw={225} className={classes.stepstitle} mt="md">
-          {step.title}
-        </Text>
-        <Text fz="sm" mt="sm" fw={225} className={classes.stepsdescription} >
-          {step.description}
-        </Text>
-      </Card>
-    ));
-  
     return (
       <div className={classes.wrapper}>  
         <Container size="lg" py="xl">
-          <Text ta="center" mt="md" fw={250} size={rem(34)} className={classes.stepsheading}>
-            Our process
-          </Text>
-
-          <SimpleGrid cols={1} spacing="xl" mt={50} ta="left" breakpoints={[{ maxWidth: 'md', cols: 1 }]}>
-            {steps}
-          </SimpleGrid>
-
           <Text className={classes.featuresdescription} ta="center" mt="md" fw={250} size={rem(34)}>
             Relax and let those emails do their thing; we&apos;ll swoop in to tidy up the mess whenever you want
           </Text>
   
-          <SimpleGrid cols={4} spacing="xl" mt={50} breakpoints={[{ maxWidth: 'md', cols: 1 }]}>
+          <SimpleGrid cols={4} spacing="xl" mt={65} breakpoints={[{ maxWidth: 'md', cols: 1 }]}>
             {features}
           </SimpleGrid>
         </Container>
