b456d8ac1f91cb4300b6d45901fb55f6748e6dc9
Shayan Arman <shayan.arman2@gmail.com>
2023-08-26 14:21:56 +0200

    got padding down for TextPlusImage

--- src/Components/TextPlusImage/TextPlusImage.tsx
+++ src/Components/TextPlusImage/TextPlusImage.tsx
@@ -1,7 +1,8 @@
 import {
   Box,
   Flex,
-  Text
+  Text,
+  Title
 } from '@mantine/core';
 import React, { useEffect, useRef, useState } from 'react';
 
@@ -34,17 +35,22 @@ export default function TextPlusImage() {
   }, []);
 
   return (
-    <Flex ref={containerRef} justify={"space-between"} wrap="wrap" gap={100} ml={"10%"} mr={"20%"} style={{border: "1px solid black"}}>
-      <Flex key="text" direction="column" justify="center">
-        <Text>
-          This is some text explaining how great Zero Inbox is.
-        </Text>
-      </Flex>
-      <Flex key="image" style={{margin: isWrapped ? "0 auto" : "0", border: "1px solid black"}}>
-        <Box mih={"250px"} miw={"200px"} style={{ backgroundColor: isWrapped ? "orange" : "black"}}>
+    <Box w="100%" mr="auto" ml="auto" style={{maxWidth: "1080px"}}>
+      <Flex ref={containerRef} justify={"space-between"} p="88px 24px 56px 24px" wrap="wrap" gap={100} align="center" style={{border: "1px solid black"}}>
+        <Flex key="text" direction="column" justify="center">
+          <Text>
+            <Title>
+              AI does not mean automatic.
+            </Title>
+            Zero AI shows you bunches of emails. You choose what to do.
+          </Text>
+        </Flex>
+        <Flex key="image" style={{margin: isWrapped ? "0 auto" : "0", border: "1px solid black"}}>
+          <Box mih={"250px"} miw={"200px"} style={{ backgroundColor: isWrapped ? "orange" : "black"}}>
 
-        </Box>
-      </Flex>
+          </Box>
+        </Flex>
     </Flex>
+    </Box>
   );
 }
