b1874af65ee92429deb09dacc379e1815a55cb11
Shayan Arman <shayan.arman2@gmail.com>
2023-08-29 17:37:03 +0200

    footer

--- src/Components/Footer/Footer2.tsx
+++ src/Components/Footer/Footer2.tsx
@@ -7,8 +7,8 @@ import Image from "next/image";
  */
 export default function Footer2() {
   return (
-    <Footer height={80} withBorder={false}>
-      <Flex h={"inherit"} justify="center" align="center">
+    <Footer height={80} style={{backgroundColor: "var(--landing-background)"}} withBorder={false}>
+      <Flex h={"inherit"} justify="center" align="center" style={{backgroundColor: "transparent"}}>
         <Image width={150} height={40} alt="zero" src="/justLogo.svg" />
       </Flex>
     </Footer>
