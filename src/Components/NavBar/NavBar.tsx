5b6cb4a8e2f692493a35d169d3f7865107d7ee19
Shayan Arman <shayan.arman2@gmail.com>
2023-09-02 16:31:35 +0200

    navBar: simple buttons

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,14 +1,15 @@
-import { createStyles, Box } from "@mantine/core";
+import { createStyles, Flex, Button } from "@mantine/core";
 import { HEADER_HEIGHT } from "../ZeroHeader/ZeroHeader";
 
 const useStyles = createStyles(
     (theme) => ({
       container: {
-          position: "fixed", 
-          left: 0, 
-          top: HEADER_HEIGHT, 
-          zIndex: 8000, 
-          backgroundColor: "var(--landing-blur)"
+        position: "fixed", 
+        left: 0, 
+        rowGap: 5,
+        top: HEADER_HEIGHT, 
+        zIndex: 8000, 
+        backgroundColor: "var(--landing-blur)"
       }
     })
 )
@@ -21,10 +22,15 @@ export default function NavBar({opened}: { opened: boolean }) {
     }
 
     return (
-      <Box 
+      <Flex 
+        direction="column"
         h={`100%`} 
         w={"100%"} 
-        className={classes.container}>      
-      </Box>
+        className={classes.container}> 
+        <Button>First</Button>
+        <Button>Second</Button>
+        <Button>Third</Button>
+        <Button>Fourth</Button>
+      </Flex>
     );
 }
