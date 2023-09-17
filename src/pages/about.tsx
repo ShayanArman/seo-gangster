dfa5ecb3af07fd8453304178dd9221825034ed49
Shayan Arman <shayan.arman2@gmail.com>
2023-09-17 12:44:29 -0400

    about looks even better

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,17 @@
-import { createStyles, Flex, Title, Text } from "@mantine/core";
+import { createStyles, Flex, Title, Text, Space } from "@mantine/core";
 
 
 const useStyles = createStyles((theme) => ({
   container: {
-    margin: "3rem 3rem 3rem 3rem",
+    margin: "3rem 0rem",
+  },
 
+  hero: {
+    width: "50%",
+
+    [theme.fn.smallerThan("xl")]: {
+      width: "80%"
+    }
   },
 
   title: {
@@ -13,8 +20,8 @@ const useStyles = createStyles((theme) => ({
 
   description: {
     fontFamily: "Calibri",
-    fontSize: "1rem",
-    lineHeight: "1rem"
+    fontSize: "1.5rem",
+    lineHeight: "2.5rem"
   }
 }));
 
@@ -22,14 +29,42 @@ export default function About() {
   const { classes } = useStyles();
 
   return (
-    <Flex direction="column" align={"center"} className={classes.container}>
-      <Flex direction="column" align="left" gap={10}>
+    <Flex direction="column" align={"center"} className={classes.container} gap={30}>
+      <Flex direction="column" className={classes.hero} gap={10}>
         <h1 className={classes.title}>
           The Future of Email
         </h1>
         <p className={classes.description}>
-          The goal of Zero Inbox is to simplify Email. Information overload, caused by too many emails, is simply stressful.
-          But it doesn&apos;t have to be this way.
+          The goal of ZeroInbox.ai is to simplify Email. Information overload, caused by too many emails, has become unmanageable.
+          But it doesn&apos;t have to be this way. Using AI, we can organize our emails as they come in.
+          Learning from user behavior, email can become creative and fun rather than a chore.
+        </p>
+      </Flex>
+
+      <Flex direction="column" className={classes.hero} gap={10}>
+        <h1 className={classes.title}>
+          Team
+        </h1>
+        <p className={classes.description}>
+          Our team is led by Shayan Arman, a Silicon Valley Engineer formerly at Apple.
+        </p>
+      </Flex>
+
+      <Flex direction="column" className={classes.hero} gap={10}>
+        <h1 className={classes.title}>
+          Advisory
+        </h1>
+        <p className={classes.description}>
+          We are advised by Bill York. Currently the head of Siri Communications.
+        </p>
+      </Flex>
+
+      <Flex direction="column" className={classes.hero} gap={10}>
+        <h1 className={classes.title}>
+          Join Us
+        </h1>
+        <p className={classes.description}>
+          We are currently recruiting Engineers both in AI and Software Engineering. Drop us a line with your CV <span><a style={{textDecoration: "underline"}} href="mailto:jobs@zeroinbox.ai?subject=Roles - Zero Inbox: AI Email Manager">here</a></span>.
         </p>
       </Flex>
     </Flex>
