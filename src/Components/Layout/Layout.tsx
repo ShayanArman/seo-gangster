522a1ce7ca74c76d6d4e89c87753f8d657873a11
Shayan Arman <shayan.arman2@gmail.com>
2023-10-06 11:45:37 -0700

    testing: turn back

--- src/Components/Layout/Layout.tsx
+++ src/Components/Layout/Layout.tsx
@@ -45,11 +45,11 @@ export default function Layout({ children }: { children: JSX.Element }) {
   return (
     <>
       <Head>
-        <title>Zero Inbox</title>
+        <title>Zero AI</title>
         <link rel="icon" href="/logo.ico" />
         <meta name="description" content="Clear your Email, clear your Mind." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="og:title" content={"Zero AI"} />
+        <meta name="og:title" content={"Zero Inbox"} />
       </Head>
       
       <Flex id="#top" direction="column" mih="100vh" className={classes.container}>
