70de45d94b02c90aec3d4f687f119c4071576c04
Shayan Arman <shayan.arman2@gmail.com>
2023-11-13 11:07:35 -0800

    add: GoogleAnalytics

--- src/Components/Analytics/GoogleAnalytics.tsx
+++ src/Components/Analytics/GoogleAnalytics.tsx
@@ -3,13 +3,16 @@ import Script from "next/script";
 export default function GoogleAnalytics() {
   return (
     <>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-BV8KLVYL1V" />
       <Script src="https://www.googletagmanager.com/gtag/js?id=AW-11398102909" />
       <Script id="google-analytics">
         {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'AW-11398102909'); `}
+            gtag('config', 'AW-11398102909');
+            gtag('config', 'G-BV8KLVYL1V'); 
+        `}
       </Script>
     </>
   );
