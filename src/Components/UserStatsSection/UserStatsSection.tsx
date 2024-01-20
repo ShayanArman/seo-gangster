3376f2703b90846835c796cb057aba01afa70239
Shayan Arman <shayan.arman2@gmail.com>
2024-01-19 16:00:36 -0800

    update Bethany city to raleigh

--- src/Components/UserStatsSection/UserStatsSection.tsx
+++ src/Components/UserStatsSection/UserStatsSection.tsx
@@ -51,7 +51,7 @@ const users: UserInfo[] = [
   {fullName: "Josh", city: "Austin, Texas", image: '/images/people/icons8-futurama-bender-100.png', countryImage: "/images/countries/icons8-usa-48.png", emailsCleanedCount: 49497},
   {fullName: "Jan", city: "Bern, Switzerland", image: '/images/people/icons8-homer-simpson-100.png', countryImage: "/images/countries/icons8-switzerland-48.png", emailsCleanedCount: 62787 },
   {fullName: "Alain", city: "San Francisco, CA", image: '/images/people/icons8-theodore-peterson-100.png', countryImage: "/images/countries/icons8-usa-48.png", emailsCleanedCount: 18338},
-  {fullName: "Bethany", city: "Toronto, ON", image: '/images/people/icons8-futurama-mom-100.png', countryImage: "/images/countries/icons8-usa-48.png", emailsCleanedCount: 16939},
+  {fullName: "Bethany", city: "Raleigh, NC", image: '/images/people/icons8-futurama-mom-100.png', countryImage: "/images/countries/icons8-usa-48.png", emailsCleanedCount: 16939},
   {fullName: "Sammy", city: "Brooklyn, NY", image: '/images/people/icons8-walter-white-100.png', countryImage: "/images/countries/icons8-usa-48.png", emailsCleanedCount: 15900 }]
 export default function UserStatsSection({ isVisible }: { isVisible: boolean }) {
   const { classes } = useStyles();
