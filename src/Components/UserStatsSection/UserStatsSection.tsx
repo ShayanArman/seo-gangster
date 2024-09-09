34020fc94956ce92b73dba90e63208fadf81670f
Shayan Arman <shayan.arman2@gmail.com>
2024-09-09 12:03:04 -0700

    better?

--- src/Components/UserStatsSection/UserStatsSection.tsx
+++ src/Components/UserStatsSection/UserStatsSection.tsx
@@ -10,7 +10,7 @@ const useStyles = createStyles((theme) => ({
     padding: "2rem 5rem",
 
     [theme.fn.smallerThan("md")]: {
-      padding: "2rem 1.3rem",
+      padding: "2rem 1.0rem",
     },
   },
 
@@ -41,18 +41,19 @@ const useStyles = createStyles((theme) => ({
 
 type UserInfo = {
   fullName: string,
+  testimonial: string,
   city: string,
   image: string,
   countryImage: string,
   emailsCleanedCount: number,
 }
 const users: UserInfo[] = [
-  {fullName: "Lisa", city: "Victoria, BC", image: '/images/people/icons8-hello-kitty-100.png', countryImage: "/images/countries/icons8-canada-48.png", emailsCleanedCount: 30130},
-  {fullName: "Shashank", city: "Austin, Texas", image: '/images/people/icons8-futurama-bender-100.png', countryImage: "/images/countries/icons8-usa-48.png", emailsCleanedCount: 55383},
-  {fullName: "Jan", city: "Bern, Switzerland", image: '/images/people/icons8-homer-simpson-100.png', countryImage: "/images/countries/icons8-switzerland-48.png", emailsCleanedCount: 62787 },
-  {fullName: "Marcus", city: "San Francisco, CA", image: '/images/people/icons8-theodore-peterson-100.png', countryImage: "/images/countries/icons8-usa-48.png", emailsCleanedCount: 21773},
-  {fullName: "Fabiola", city: "Mahwah, NJ", image: '/images/people/icons8-futurama-mom-100.png', countryImage: "/images/countries/icons8-usa-48.png", emailsCleanedCount: 30251},
-  {fullName: "Eric", city: "Austin, Texas", image: '/images/people/icons8-walter-white-100.png', countryImage: "/images/countries/icons8-usa-48.png", emailsCleanedCount: 65954 }]
+  {fullName: "Lisa", city: "Victoria, BC", testimonial: "Wow. Just wow.", image: '/images/people/icons8-hello-kitty-100.png', countryImage: "/images/countries/icons8-canada-48.png", emailsCleanedCount: 30130},
+  {fullName: "Shashank", city: "Austin, Texas", testimonial: "I got my entire office on this haha.", image: '/images/people/icons8-futurama-bender-100.png', countryImage: "/images/countries/icons8-usa-48.png", emailsCleanedCount: 55383},
+  {fullName: "Jan", city: "Bern, Switzerland", testimonial: "Cleaned everything up in a few minutes!", image: '/images/people/icons8-homer-simpson-100.png', countryImage: "/images/countries/icons8-switzerland-48.png", emailsCleanedCount: 62787 },
+  {fullName: "Marcus", city: "San Francisco, CA", testimonial: "Please make this for Files now.", image: '/images/people/icons8-theodore-peterson-100.png', countryImage: "/images/countries/icons8-usa-48.png", emailsCleanedCount: 21773},
+  {fullName: "Fabiola", city: "Mahwah, NJ", testimonial: "Why was I not told this existed?!", image: '/images/people/icons8-futurama-mom-100.png', countryImage: "/images/countries/icons8-usa-48.png", emailsCleanedCount: 30251},
+  {fullName: "Eric", city: "Austin, Texas", testimonial: "Gotta admit, I'm a bit addicted.", image: '/images/people/icons8-walter-white-100.png', countryImage: "/images/countries/icons8-usa-48.png", emailsCleanedCount: 65954 }]
 export default function UserStatsSection({ isVisible }: { isVisible: boolean }) {
   const { classes } = useStyles();
 
@@ -64,16 +65,19 @@ export default function UserStatsSection({ isVisible }: { isVisible: boolean }) 
             Leader Board
           </Title>
           <Text weight={300} size="xs">
-            Updated June 14th, 2024
+            Updated Septemer 9th, 2024
           </Text>
         </Flex>
-        <Flex direction="column" gap={"sm"} mt={10}>
+        <Flex direction="column" align={"center"} gap={"sm"} mt={10}>
           { users.sort((a, b) => b.emailsCleanedCount - a.emailsCleanedCount).map((user, i) => {
             return (
               <Group key={user.fullName}>
                 <Avatar src={user.image} radius="xl" size="lg" />
                 <Flex direction="column">
                   <Flex align="center" gap={5}><Text>{user.fullName}</Text><Image width={15} height={15} alt="zero" src={user.countryImage} /></Flex>
+                  <Text size="xs" c="dimmed">
+                    &quot;{user.testimonial}&quot;
+                  </Text>
                   <Text size="sm" c="dimmed" fw={400}>
                     {user.emailsCleanedCount} emails
                   </Text>
