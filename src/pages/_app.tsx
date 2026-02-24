e042de69cbe7f3b1d24265ea1f5f3dc67b988685
Shayan Arman <shayan.arman2@gmail.com>
2026-02-24 13:54:02 -0800

    update: _app to include Mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,19 @@
 import '@/styles/globals.css'
 import "@/styles/variables.css"
-import type { AppProps } from 'next/app'
 import Layout from '@/Components/Layout';
-
+import { MantineProvider } from '@mantine/core'
+import { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{ colorScheme: 'light' }}
+    >
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </MantineProvider>
+  )
 }
